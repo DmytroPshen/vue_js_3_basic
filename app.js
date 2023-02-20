5d396c0526d38f91e5a13d1fabd10acfcd6d1f07
DmytroPshen <mailforajob49@gmail.com>
2023-02-20 21:44:12 +0200

    some changes exit

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const App = {
     data() {
         return {
-            counter: 55,
-            // title: {}
-            title: 'Лічильник'
+            // counter: 0,
+            placeholderString: 'Внесіть назву нотатки',
+            title: 'Список нотаток'
         }
     }
 }
 
-const app = Vue.createApp(App)
+// const app = Vue.createApp(App)
 
-app.mount('#app')
-// Vue.createApp(App).mount('#app')
+// app.mount('#app')
+Vue.createApp(App).mount('#app')
